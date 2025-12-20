a29e085e5545d98a7243428909bff743a92faee9
Rohan Sachdeva <rohansachdeva1126dp@gmail.com>
2025-12-21 00:17:08 +0530

    common commit

--- Graphs/new.cpp
+++ Graphs/new.cpp
@@ -27,105 +27,37 @@ void buildGraph(graph &g){
 
   for(int i=0;i<m;i++){
     int u,v;cin>>u>>v;
-    g.addEdge(u,v,1);
+    g.addEdge(u,v,0);
   }
 }
 
-bool dfsCycle(int node,vector<vector<int>>& adj,vector<int>& vis, vector<int>& pathVis){
+
+void shortestDist(int src,vector<vector<int>>& adj){
   int n=adj.size()-1;
+  vector<int> dist(n,INT_MAX);
+  queue<pair<int,int>> q;
+  dist[src]=0;
+  q.push({src,0});
 
-  vis[node]=1;
-  pathVis[node]=1;
-
-  for(auto x:adj[node]){
-    if(!vis[x]){
-       if(dfsCycle(x,adj,vis,pathVis)==true){
-        return true;
-       }
-    }
-    else if(vis[x]==1 && pathVis[x]==1) return true;
-  }
-  pathVis[node]=0;
-  return false;
-}
-bool isCycleDirected(vector<vector<int>>& adj){
-  int n=adj.size()-1;
-  vector<int> vis(n+1,0);
-  vector<int> pathVis(n+1,0);
-  for(int i=1;i<=n;i++){
-    if(!vis[i]) {
-      if(dfsCycle(i,adj,vis,pathVis)==true){
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-void dfsTopo(int node,vector<vector<int>>& adj, vector<int>& vis,stack<int>& st){
-  vis[node]=1;
-  for(auto x:adj[node]){
-    if(!vis[node]) dfsTopo(x,adj,vis,st);
-  }
-  st.push(node);
-}
-void topoDFS(vector<vector<int>>& adj){
-  int n=adj.size()-1;
-  vector<int> vis(n+1,0);
-  stack<int> st;
-  for(int i=1;i<=n;i++){
-    if(!vis[i]) dfsTopo(i,adj,vis,st);
-  }
-  
-  vector<int> topoAns;
-  while(!st.empty()){
-    int top = st.top();st.pop();
-    topoAns.push_back(top);
-  }
-
-  for(auto x:topoAns){
-    cout<<x<<" ";
-  }
-}
-
-
-bool isCycleDirectedBFS(vector<vector<int>>& adj){
-  int n=adj.size()-1;
-  vector<int> inDegree(n+1,0);
-
-  for(int i=1;i<=n;i++){
-    for(auto x:adj[i]){
-      inDegree[x]++;
-    }
-  }
-  queue<int> q;
-  for(int i=1;i<=n;i++){
-    if(inDegree[i]==0){
-      q.push(i);
-    }
-  }
-  vector<int> ans;
   while(!q.empty()){
-    int front=q.front();q.pop();
-    ans.push_back(front);
+    auto front=q.front();q.pop();
+    
+    int node=front.first, d=front.second;
 
-    for(auto x:adj[front]){
-      inDegree[x]--;
-      if(inDegree[x]==0){
-        q.push(x);
+    for(auto x:adj[node]){
+      if(dist[x] > d+1) {
+        dist[x]=d+1;
+        q.push({x,d+1});
       }
     }
   }
 
-  if(ans.size()!=n) return true;
-  else return false;
+  for(auto x:dist) cout<<x<<" ";
 }
-
-
 
 int main(){
   graph g;
   buildGraph(g);
-  
-  cout<<isCycleDirectedBFS(g.adj);
+
+  shortestDist(0,g.adj);
 }
